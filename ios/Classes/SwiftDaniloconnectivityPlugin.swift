be81088123184658536b539005bf9c942ffb15ff
Danilo Ganzella <dganzella@2mundos.net>
2019-07-25 17:13:08 -0300

    start adapting

--- ios/Classes/SwiftDaniloconnectivityPlugin.swift
+++ ios/Classes/SwiftDaniloconnectivityPlugin.swift
@@ -1,18 +1,20 @@
 import Flutter
 import UIKit
+import Reachability
 
 public class SwiftDaniloconnectivityPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
-
+    
     public static var eventSink: FlutterEventSink?
-
+    
     public static func register(with registrar: FlutterPluginRegistrar) {
+        
+        let channel = FlutterMethodChannel(name: "plugins.flutter.io/connectivity", binaryMessenger: registrar.messenger())
         let instance = SwiftDaniloconnectivityPlugin()
-
-        let channel = FlutterMethodChannel(name: "plugins.flutter.io/connectivity", binaryMessenger: registrar?.messenger())
         registrar.addMethodCallDelegate(instance, channel: channel)
-
-        let streamChannel = FlutterEventChannel(name: "plugins.flutter.io/connectivity_status", binaryMessenger: registrar?.messenger())
-        streamChannel.streamHandler = instance
+        
+        let messageChannel = FlutterEventChannel(name: "plugins.flutter.io/connectivity_status", binaryMessenger: registrar.messenger())
+        messageChannel.setStreamHandler(instance)
+        
     }
 
     func findNetworkInfo(_ key: String?) -> String? {
@@ -66,76 +68,44 @@ public class SwiftDaniloconnectivityPlugin: NSObject, FlutterPlugin, FlutterStre
         freeifaddrs(interfaces)
 
         return address
-}
-
-
-//  Converted to Swift 5 by Swiftify v5.0.7505 - https://objectivec2swift.com/
-func status(from reachability: Reachability?) -> String? {
-
-    let status = reachability?.currentReachabilityStatus()
-    switch status {
-        case NotReachable?:
-            return "none"
-        case ReachableViaWiFi?:
-            return "wifi"
-        case ReachableViaWWAN?:
-            return "mobile"
-        default:
-            break
+        
     }
-}
 
-
-
-
-
-
-
-
-
-//  Converted to Swift 5 by Swiftify v5.0.7505 - https://objectivec2swift.com/
-func handle(_ call: FlutterMethodCall?, result: FlutterResult) {
-    if (call?.method == "check") {
-        // This is supposed to be quick. Another way of doing this would be to
-        // signup for network
-        // connectivity changes. However that depends on the app being in background
-        // and the code
-        // gets more involved. So for now, this will do.
-        result(status(from: Reachability()))
-    } else if (call?.method == "wifiName") {
-        result(getWifiName())
-    } else if (call?.method == "wifiBSSID") {
-        result(getBSSID())
-    } else if (call?.method == "wifiIPAddress") {
-        result(getWifiIP())
-    } else {
-        result(FlutterMethodNotImplemented)
+    func status(from reachability: Reachability) -> String? {
+        return  reachability.connection.description;
     }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if (call.method == "check") {
+            result(status(from: Reachability()))
+        } else if (call.method == "wifiName") {
+            result(getWifiName())
+        } else if (call.method == "wifiBSSID") {
+            result(getBSSID())
+        } else if (call.method == "wifiIPAddress") {
+            result(getWifiIP())
+        } else {
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    func onReachabilityDidChange(_ notification: Notification?) {
+        let curReach = notification?.object as? Reachability
+        eventSink(status(from: curReach))
+    }
+
+
+    public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+        self.eventSink = eventSink
+        NotificationCenter.default.addObserver(self, selector: #selector(onReachabilityDidChange(_:)), name: kReachabilityChangedNotification, object: nil)
+        Reachability().startNotifier()
+        return nil
+    }
+
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        
+    }
+
+
 }
 
-func onReachabilityDidChange(_ notification: Notification?) {
-    let curReach = notification?.object as? Reachability
-    eventSink(status(from: curReach))
-}
-
-
-func onListen(withArguments arguments: Any?, eventSink: FlutterEventSink) -> FlutterError? {
-    self.eventSink = eventSink
-    NotificationCenter.default.addObserver(self, selector: #selector(onReachabilityDidChange(_:)), name: kReachabilityChangedNotification, object: nil)
-    Reachability().startNotifier()
-    return nil
-}
-
--( as? FlutterError)!
-do {
-    Reachability().stopNotifier()
-    NotificationCenter.default.removeObserver(self)
-    eventSink = nil
-    return nil
-}
-- (FlutterError*)onCancelWithArguments:(id)arguments {
-  [[Reachability reachabilityForInternetConnection] stopNotifier];
-  [[NSNotificationCenter defaultCenter] removeObserver:self];
-  _eventSink = nil;
-  return nil;
-}
