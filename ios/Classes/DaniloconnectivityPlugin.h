2e0be0a7e35d2783d02b7fb1f861218937e03337
Danilo Ganzella <dganzella@2mundos.net>
2019-07-25 18:05:25 -0300

    simple try solved

--- ios/Classes/DaniloconnectivityPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface DaniloconnectivityPlugin : NSObject<FlutterPlugin>
-@end
--- ios/Classes/DaniloconnectivityPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "DaniloconnectivityPlugin.h"
-#import <daniloconnectivity/daniloconnectivity-Swift.h>
-
-@implementation DaniloconnectivityPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftDaniloconnectivityPlugin registerWithRegistrar:registrar];
-}
-@end
